1b3da6a52ebeabda83fe7f15e980b88688404130
Tristan <50113330+TrisDooley@users.noreply.github.com>
2024-07-26 15:22:00 -0700

    adding real inputs fake pins

---
+++ Main_Board/CAN_Constants.h
@@ -0,0 +1,5 @@
+#define STEERING_CAN_ID 0x20
+#define CCL_PIN 6
+#define DCL_PIN 7
+#define FAULT_PIN 8
+#define ESTOP_PIN 9
--- Main_Board/Main_Board.ino
+++ Main_Board/Main_Board.ino
@@ -1,11 +1,23 @@
 //API reference: https://github.com/sandeepmistry/arduino-CAN/blob/master/API.md
 
 #include <CAN.h>
+#include "CAN_Constants.h"
 
 #define TX_GPIO_NUM 5
 #define RX_GPIO_NUM 4
 
 bool leftTurn;
+bool rightTurn;
+bool mainPower;
+float throttle;
+float ccl;
+float dcl;
+bool fault;
+bool estop;
+bool brake;
+bool cruiseControl;
+bool hazards;
+
 byte steeringData[8];
 
 void setup() {
@@ -20,12 +32,22 @@ void loop() {
 
 void readInputs() {
   leftTurn = steeringData[0] & 0x01;
+  rightTurn = steeringData[0] & 0x02;
+  mainPower = steeringData[0] & 0x04;
+  throttle = steeringData[1]/255.0;
+  ccl = analogRead(CCL_PIN)/255.0;
+  dcl = analogRead(DCL_PIN)/255.0;
+  fault = digitalRead(FAULT_PIN);
+  estop = digitalRead(ESTOP_PIN);
+  brake = steeringData[0] & 0x08;
+  cruiseControl = steeringData[0] & 0x10;
+  hazards = steeringData[0] & 0x20;
 }
 
 void readCAN(int packetSize) {
   int dataID = CAN.packetId();
   int position = 0;
-  if(dataID == 0x20) {
+  if(dataID == STEERING_CAN_ID) {
     while(CAN.available()) {
       steeringData[position] = CAN.read();
       position += 1;
