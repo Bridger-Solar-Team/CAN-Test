9e794f5fe639df3477dd04845eea4a3d7f252b76
Tristan <trisdooley@gmail.com>
2024-12-01 22:22:31 -0700

    sd card works
    added slashes to file names and found out that append exists

--- sdCardLoggerTest/sdCardLoggerTest.ino
+++ sdCardLoggerTest/sdCardLoggerTest.ino
@@ -1,27 +1,52 @@
 #include <SD.h>
 #include <SPI.h>
 
-#define CHIP_SELECT_PIN 10
+#define CHIP_SELECT_PIN 17
+#define ODD_PIN_BEHAVIOUR 5
 
-File dataFile;
 String dataString;
 
 void setup() {
-
-  SD.begin();
-  dataFile = SD.open("data.txt", FILE_WRITE);
+  Serial.begin(115200);
 
   pinMode(CHIP_SELECT_PIN, OUTPUT);
+  pinMode(ODD_PIN_BEHAVIOUR, OUTPUT);
+
+  while (!SD.begin(CHIP_SELECT_PIN)) {
+    Serial.println(F("SD CARD FAILED, OR NOT PRESENT!"));
+    delay(1000);
+  }
+  File dataFile;
+  dataFile = SD.open("/data.txt", FILE_WRITE);
+  while (!dataFile) {
+    Serial.println("FILE NOT OPEN");
+    dataFile = SD.open("/data.txt", FILE_WRITE);
+    delay(1000);
+  }
+
+  dataFile.close();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   logData();
+  delay(1000);
 }
 
 void logData() {
   dataString += "Current Time: ";
   dataString += millis();
-  dataFile.println(dataString);
+
+  File dataFile;
+  dataFile = SD.open("/data.txt", FILE_APPEND);
+  if (!dataFile) {
+    Serial.println("FAILED TO OPEN FILE TO WRITE");
+  } else {
+    dataFile.println(dataString);
+    dataFile.close();
+
+    Serial.print(dataString);
+  }
+
   dataString = "";
 }
